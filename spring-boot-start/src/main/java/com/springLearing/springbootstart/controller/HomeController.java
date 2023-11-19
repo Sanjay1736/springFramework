01352a2ccd36e40c0534797fbcfa76113d64d70a
Sanjay1736 <92837525+Sanjay1736@users.noreply.github.com>
2023-11-19 23:06:19 +0530

    SpringFamework

---
+++ spring-boot-start/src/main/java/com/springLearing/springbootstart/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.springLearing.springbootstart.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HomeController {
+    @RequestMapping("/")
+    public String home(){
+        return "hello world!";
+    }
+
+}
